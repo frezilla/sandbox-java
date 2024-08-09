0e4b58cd97d74b3dab8194c11223755eaf092b67
Frezilla <fb.info@mailo.com>
2024-08-09 15:21:00 +0200

    Finalisation de l'algorithme de génération du labyrinthe

--- maze/src/main/java/eu/frezilla/maze/generator/IterativeImplementationGenerator.java
+++ maze/src/main/java/eu/frezilla/maze/generator/IterativeImplementationGenerator.java
@@ -19,22 +19,18 @@ public final class IterativeImplementationGenerator implements MazeGenerator {
     
     @Override
     public Maze generate(int width, int height) {
+        Random rand = new Random();
+        
         Maze maze = new Maze(width, height);
         
-        List<IterativeCell> unvisitedCells = new ArrayList<>(width * height);
         List<IterativeCell> allCells = new ArrayList<>(width * height);
         Map<Integer, List<IterativeCell>> cellsByGroupIdMap = new HashMap<>();
         
         for (int j = 0; j < maze.getHeight(); j++) {
             for (int i = 0 ; i < maze.getWidth(); i++) {
                 int groupId = (i + j * maze.getHeight());
-                IterativeCell iterativeCell = 
-                        new IterativeCell(
-                                maze.getCell(i, j), 
-                                i, 
-                                j, 
-                                groupId
-                        );
+                IterativeCell iterativeCell = new IterativeCell(maze.getCell(i, j), i, j, groupId);
+                
                 List<IterativeCell> cells = new ArrayList<>();
                 cells.add(iterativeCell);
                 cellsByGroupIdMap.put(groupId, cells);
@@ -42,52 +38,62 @@ public final class IterativeImplementationGenerator implements MazeGenerator {
                 allCells.add(iterativeCell);
             }
         }
-        unvisitedCells.addAll(allCells);
         
-        Random rand = new Random();
+        List<Integer> groupIdList = new ArrayList<>(cellsByGroupIdMap.keySet());
         
-        while (!unvisitedCells.isEmpty()) {
-            int indexCell = rand.nextInt(unvisitedCells.size());
-            IterativeCell iterativeCell = unvisitedCells.remove(indexCell);
+        while (groupIdList.size() > 1) {
+            int groupId = groupIdList.get(rand.nextInt(groupIdList.size()));
             
-            Cell cell = iterativeCell.getCell();
-            int i = iterativeCell.getI();
-            int j = iterativeCell.getJ();
-            int groupId = iterativeCell.getGroupId();
+            List<IterativeCell> unvisitedCells = new ArrayList<>(cellsByGroupIdMap.get(groupId));
             
-            IterativeCell westCell = getNeighboringCell(i, j, Direction.WEST, allCells, width, height);
-            IterativeCell eastCell = getNeighboringCell(i, j, Direction.EAST, allCells, width, height);
-            IterativeCell northCell = getNeighboringCell(i, j, Direction.NORTH, allCells, width, height);
-            IterativeCell soutCell = getNeighboringCell(i, j, Direction.SOUTH, allCells, width, height);
+            while (!unvisitedCells.isEmpty()) {
+                IterativeCell currentCell = unvisitedCells.remove(rand.nextInt(unvisitedCells.size()));
+            
+                Cell cell = currentCell.getCell();
+                int i = currentCell.getI();
+                int j = currentCell.getJ();
+            
+                IterativeCell westCell = getNeighboringCell(i, j, Direction.WEST, allCells, width, height);
+                IterativeCell eastCell = getNeighboringCell(i, j, Direction.EAST, allCells, width, height);
+                IterativeCell northCell = getNeighboringCell(i, j, Direction.NORTH, allCells, width, height);
+                IterativeCell soutCell = getNeighboringCell(i, j, Direction.SOUTH, allCells, width, height);
                         
-            List<Direction> directions = new ArrayList<>();
-            directions.addAll(Arrays.asList(Direction.values()));
+                List<Direction> directions = new ArrayList<>(Arrays.asList(Direction.values()));
             
-            if (i == 0 || cell.hasWestCell() || (westCell != null && Objects.equals(westCell.getGroupId(), groupId))) directions.remove(Direction.WEST);
-            if (i + 1 == width || cell.hasEastCell() || (eastCell != null && Objects.equals(eastCell.getGroupId(), groupId))) directions.remove(Direction.EAST);
-            if (j == 0 || cell.hasNorthCell() || (northCell != null && Objects.equals(northCell.getGroupId(), groupId))) directions.remove(Direction.NORTH);
-            if (j +1 == height || cell.hasSouthCell() || (soutCell != null && Objects.equals(soutCell.getGroupId(), groupId))) directions.remove(Direction.SOUTH);
+                if (i == 0 || cell.hasWestCell() || (westCell != null && Objects.equals(westCell.getGroupId(), groupId))) directions.remove(Direction.WEST);
+                if (i + 1 == width || cell.hasEastCell() || (eastCell != null && Objects.equals(eastCell.getGroupId(), groupId))) directions.remove(Direction.EAST);
+                if (j == 0 || cell.hasNorthCell() || (northCell != null && Objects.equals(northCell.getGroupId(), groupId))) directions.remove(Direction.NORTH);
+                if (j +1 == height || cell.hasSouthCell() || (soutCell != null && Objects.equals(soutCell.getGroupId(), groupId))) directions.remove(Direction.SOUTH);
             
-            if (!directions.isEmpty()) {
-                int indexDirection = rand.nextInt(directions.size());
-                Direction direction = directions.get(indexDirection);
-                maze.openCrossing(i, j, direction);
+                if (!directions.isEmpty()) {
+                    Direction direction = directions.get(rand.nextInt(directions.size()));
+                    maze.openCrossing(i, j, direction);
                 
-                switch (direction) {
-                    case EAST:
-                        eastCell.getGroupId();
-                        break;
-                    case NORTH:
-                        northCell.getGroupId();
-                        break;                        
-                    case SOUTH:
-                        soutCell.getGroupId();
-                        break;
-                    case WEST:
-                        westCell.getGroupId();
-                        break;                        
-                    default:
-                        throw new AssertionError();
+                    int directionGroupId;
+                    switch (direction) {
+                        case EAST:
+                            directionGroupId = Objects.requireNonNull(eastCell).getGroupId();
+                            break;
+                        case NORTH:
+                            directionGroupId = Objects.requireNonNull(northCell).getGroupId();
+                            break;                        
+                        case SOUTH:
+                            directionGroupId = Objects.requireNonNull(soutCell).getGroupId();
+                            break;
+                        case WEST:
+                            directionGroupId = Objects.requireNonNull(westCell).getGroupId();
+                            break;                        
+                        default:
+                            throw new AssertionError();
+                    }
+                
+                    List<IterativeCell> directionCells = cellsByGroupIdMap.get(directionGroupId);
+                    directionCells.forEach(dc -> dc.setGroupId(groupId));
+                    cellsByGroupIdMap.get(groupId).addAll(directionCells);
+                    
+                    cellsByGroupIdMap.remove(directionGroupId);
+                    
+                    groupIdList = new ArrayList<>(cellsByGroupIdMap.keySet());
                 }
             }
         }
